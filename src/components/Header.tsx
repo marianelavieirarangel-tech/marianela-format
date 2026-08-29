6873b68aefe989a1745ffa79e4d110c21a86973d
jurgen <vieirarangelmarianela@gmail.com>
2026-08-28 19:57:07 -0500

    Align header below promo bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -92,7 +92,7 @@ export default function Header({
         className={`${
           isHome && !scrolled
             ? 'fixed left-0 right-0 top-[37px] text-sand-50'
-            : 'fixed left-0 right-0 top-0 bg-sand-50 shadow-[0_1px_0_0_rgba(26,22,17,0.08)] text-ink-800'
+            : 'fixed left-0 right-0 top-[37px] bg-sand-50 shadow-[0_1px_0_0_rgba(26,22,17,0.08)] text-ink-800'
         } z-40 transition-[top,background-color,color,box-shadow] duration-500 ease-out`}
       >
           <div className="relative flex h-[72px] items-center lg:h-[84px]">
